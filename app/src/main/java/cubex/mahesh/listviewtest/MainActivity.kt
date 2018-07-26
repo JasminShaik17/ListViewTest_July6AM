4d9cdbc5db0adcdc8a4f924b9b64a102c9eb82be
maheshthippala <mahesh.cubex@gmail.com>
2018-07-26 18:08:22 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/listviewtest/MainActivity.kt
@@ -0,0 +1,31 @@
+package cubex.mahesh.listviewtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        readFiles()
+    }
+
+    fun readFiles( )
+    {
+        var path = "/storage/sdcard0/"
+        var f = File(path)
+        if(!f.exists())
+        {
+            path = "/storage/emulated/0/"
+            f = File(path)
+        }
+        var files = f.list()
+        var myadapter = ArrayAdapter<String>(this@MainActivity,
+             android.R.layout.simple_list_item_single_choice, files )
+        lview.adapter = myadapter
+    }
+}
